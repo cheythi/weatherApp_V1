5e43c29317199a86da5199e33a63e0c860d85b1e
cheythi <chethidasanayaka@gmail.com>
2023-06-25 12:08:12 +0530

    default controller

---
+++ application/controllers/Weather_Controller.php
@@ -0,0 +1,30 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Weather_Controller extends CI_Controller
+{
+    public function index()
+    {
+        $this->load->model('Weather_Model');
+        $this->load->helper('url');
+
+        $latitude  = $this->input->get('latitude');
+        $longitude = $this->input->get('longitude');
+        // $unit      = $this->input->get('unit');
+
+        // if (!$latitude || !$longitude) 
+        // {
+        //     echo 'test';
+        //     return;
+        // }
+
+        $weather = $this->Weather_Model->getWeatherData($latitude, $longitude);
+
+        $data['weather'] = $weather;
+
+        $this->load->view('weather_view', $data);
+
+    }
+    
+}
+
