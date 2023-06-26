83e27222e74e21cb413099f2fb9ab92703eeae6e
cheythi <chethidasanayaka@gmail.com>
2023-06-26 23:22:22 +0530

    modification

--- application/views/header.php
+++ application/views/header.php
@@ -9,9 +9,11 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <!-- Custom CSS -->
     <link rel="stylesheet" href="<?php echo base_url('src/css/custom.css'); ?>">
-    <!-- Google fonts -->
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Sofia">
+    <!-- fontawesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
-    <title>Weather App</title>
+    <title>Weather Finder</title>
+    <link rel="icon" type="png" href="<?php echo base_url('src/img/icon01.png'); ?>">
+
   </head>
-  <body>
+
